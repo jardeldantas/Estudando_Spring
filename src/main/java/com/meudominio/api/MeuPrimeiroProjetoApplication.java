da8e0ec62e5cb8c5923ad2c5c18daf40e114ac34
jardeldantas <jarde.dantas@outlook.com>
2019-08-19 00:32:50 -0300

    Versão inicial do meu primeiro projeto java com Spring Boot

---
+++ src/main/java/com/meudominio/api/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.meudominio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+
+}
